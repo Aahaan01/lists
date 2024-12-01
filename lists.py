54cbf11c3020f5b761eb81249e60ff76b892eed7
Aahaan01 <gargaahaan01@gmail.com>
2024-12-01 11:47:05 +0530

    ghhjbbhjbhj

---
+++ lists.py
@@ -0,0 +1,11 @@
+list1=[]
+#print(list1)
+
+list2=[1,"aahaan",True,4.5]
+#print(list2)
+
+list3=list2*3
+#print(list3)
+
+list4=list2[-1::-1]
+print(list4)
---
+++ playwithlists.py
@@ -0,0 +1,7 @@
+list1=[3,6,4,9]
+sum=0
+for i in list1:
+    sum=sum+i
+print(sum)
+average=sum/len(list1)
+print("Average is", average)
